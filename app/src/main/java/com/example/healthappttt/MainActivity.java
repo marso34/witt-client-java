4a8f451bd3cee70f154bc275ded7b14a67a6a81d
marso34 <mars102aq@gmail.com>
2022-11-15 10:22:52 +0900

    timer renewal3

--- app/src/main/java/com/example/healthappttt/MainActivity.java
+++ app/src/main/java/com/example/healthappttt/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     private Timer TimerCall;
     TimerTask timerTask;
 
+    private LinearLayout timerLayout;
+
     private Button StartBtn, StopBtn, PauseBtn;
     private Button TimerStBtn, TimerReBtn, MinuteBtn, MinuteHarfBtn, SecBtn, SecBtn2;
     private TextView StopWatchTextView;
@@ -30,16 +33,23 @@ public class MainActivity extends AppCompatActivity {
 
     private int timer;
 
-    private long startTime;
-    private long stopTime;
+    private long startTime; // 스톱워치 시작 버튼 누른 시간
+    private long pauseTime; // 일시정지 버튼 누른 시간
+
+    private int runTime; // 운동 시간
+    private int xrunTime; // 운동 시간
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TimerCall = new Timer();
+        startTime = 0;
+        pauseTime = 0;
+        runTime = 0;
 
+        TimerCall = new Timer();
+        timerLayout = (LinearLayout) findViewById(R.id.timer);
         StartBtn = (Button) findViewById(R.id.startBtn); // 스톱워치
         StopBtn = (Button) findViewById(R.id.stopBtn);
         PauseBtn = (Button) findViewById(R.id.pauseBtn);
@@ -55,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
         PauseBtn.setVisibility(View.GONE);
         StopBtn.setVisibility(View.GONE);
+        timerLayout.setVisibility(View.GONE);
 
         // 스톱워치
         StartBtn.setOnClickListener(new View.OnClickListener() {
@@ -63,8 +74,10 @@ public class MainActivity extends AppCompatActivity {
                 v.setVisibility(View.GONE);
                 PauseBtn.setVisibility(View.VISIBLE);
                 StopBtn.setVisibility(View.VISIBLE);
+                timerLayout.setVisibility(View.VISIBLE);
 
                 startTime = System.currentTimeMillis();
+                isRunning = true;
 
                 timerTask = new TimerTask() {
                     @Override
@@ -72,10 +85,9 @@ public class MainActivity extends AppCompatActivity {
                         someWork();
                     }
                 };
-                TimerCall.schedule(timerTask,0,10); // 0.5초
+                TimerCall.schedule(timerTask,0,10); // 0.01초
             }
         });
-
 
         PauseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,12 +96,32 @@ public class MainActivity extends AppCompatActivity {
 
                 if (isRunning) {
                     PauseBtn.setText("일시정지");
+                    startTime += (System.currentTimeMillis() - pauseTime);
+                    timerLayout.setVisibility(View.VISIBLE);
                 } else {
                     PauseBtn.setText("재시작");
-                    stopTime = System.currentTimeMillis();
+                    pauseTime = System.currentTimeMillis();
+                    timerLayout.setVisibility(View.GONE);
                 }
             }
         });
+
+        StopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRunning = false;
+                TimerRunning = false;
+                startTime = 0;
+                pauseTime = 0;
+                timer = 0;
+
+                StartBtn.setVisibility(View.VISIBLE);
+                PauseBtn.setVisibility(View.GONE);
+                StopBtn.setVisibility(View.GONE);
+//                StopWatchTextView.setText("00:00:00");
+            }
+        });
+
         // 여기서부터는 타이머
         TimerStBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -147,8 +179,7 @@ public class MainActivity extends AppCompatActivity {
         Message msg = new Message();
 
         if (isRunning)
-            msg.arg1 = (int)(System.currentTimeMillis() - startTime);
-
+            runTime = (int)(System.currentTimeMillis() - startTime);
 
         handler.sendMessage(msg);
     }
@@ -157,12 +188,12 @@ public class MainActivity extends AppCompatActivity {
     Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            int mSec = msg.arg1 % 1000;
-            int sec = (msg.arg1 / 1000) % 60;
-            int min = (msg.arg1 / 1000) / 60 % 60;
-            int hour = (msg.arg1 / 1000) / (60 * 60);
+            int mSec = runTime % 1000;
+            int sec = (runTime / 1000) % 60;
+            int min = (runTime / 1000) / 60 % 60;
+            int hour = (runTime / 1000) / (60 * 60);
 
-            if (TimerRunning && timer > 0 && (msg.arg1 % 1000) == 0) // msg.arg1 % 100 == 0
+            if (TimerRunning && timer > 0 && xrunTime/1000 < runTime/1000)
                 timer--; // 1초에 1씩 줄어들게
 
             int sec2 = timer % 60;
@@ -176,7 +207,9 @@ public class MainActivity extends AppCompatActivity {
                 TimerStBtn.setText("시작");
             }
 
-            @SuppressLint("DefaultLocale") String result = String.format("%02d:%02d:%02d", hour, min, sec);
+            xrunTime = runTime;
+
+            @SuppressLint("DefaultLocale") String result = String.format("%02d:%02d:%02d", hour, min, sec, mSec);
             @SuppressLint("DefaultLocale") String result2 = String.format("%02d:%02d:%02d", hour2, min2, sec2);
             StopWatchTextView.setText(result);
             TimerTextView.setText(result2);
