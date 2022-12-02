cfc3a4c4f1d6806d0d8d6f24669ab93c35aad370
marso34 <mars102aq@gmail.com>
2022-12-02 19:04:31 +0900

    dddddd

--- app/src/main/java/com/example/healthappttt/MainActivity.java
+++ app/src/main/java/com/example/healthappttt/MainActivity.java
@@ -1,27 +1,10 @@
 package com.example.healthappttt;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.net.wifi.aware.DiscoverySession;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
