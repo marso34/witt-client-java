90ec8e9ee055298fabd0c20b56139cfd561df28a
hwstar-1204 <115150798+hwstar-1204@users.noreply.github.com>
2022-12-02 21:11:37 +0900

    calender photo update

--- app/src/main/java/com/example/healthappttt/CalenderSubActivity.java
+++ app/src/main/java/com/example/healthappttt/CalenderSubActivity.java
@@ -1,20 +1,35 @@
 package com.example.healthappttt;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import androidx.core.content.PermissionChecker;
+import androidx.fragment.app.FragmentActivity;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Calendar;
 
 
@@ -23,7 +38,7 @@ public class CalenderSubActivity extends Activity {
     Button btn_sub;
     EditText mMemoEdit = null;
     CalenderTextFileManager mTextFileManager = new CalenderTextFileManager(this);
-
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -37,9 +52,23 @@ public class CalenderSubActivity extends Activity {
             }
         });
 
-        mMemoEdit = (EditText)findViewById(R.id.memo_edit);
+        //이미지
+       imageView = (ImageView) findViewById(R.id.image1);
+        //profile imageView 에 사진 넣는 경우
 
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,0);
+
+            }
+        });
+
+
+        mMemoEdit = (EditText)findViewById(R.id.memo_edit);
         diaryTextView2=findViewById(R.id.diaryTextView2);
+
 
     }
 
@@ -115,7 +144,50 @@ public class CalenderSubActivity extends Activity {
         newFragment.show(getFragmentManager(), "datePicker");
     }
     //---
+    //이미지뷰 클릭시 intent 실행
+    @SuppressLint("Range")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //이미지 뷰를 클릭하면 시작되는 함수
 
-    //이미지
+        if(requestCode== 0 && resultCode==RESULT_OK && data!=null) {
+            //response에 getData , return data 부분 추가해주어야 한다
+
+            Uri selectedImage = data.getData();
+            Uri photoUri = data.getData();
+            Bitmap bitmap = null;
+            //bitmap 이용
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),photoUri);
+
+                bitmap = rotateImage(bitmap, 90);
+                //사진이 돌아가 있는 경우 rotateImage 함수 이용해서 사진 회전 가능
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //이미지뷰에 이미지 불러오기
+
+            imageView.setImageBitmap(bitmap);
+
+            //아래 커서 이용해서 사진의 경로 불러오기
+            Cursor cursor = getContentResolver().query(Uri.parse(selectedImage.toString()), null, null, null, null);
+            assert cursor != null;
+            cursor.moveToFirst();
+            String mediaPath = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
+            Log.d("경로 확인 >> ", "$selectedImg  /  $absolutePath");
+
+        }else{
+            Toast.makeText(this, "사진 업로드 실패", Toast.LENGTH_LONG).show();
+        }
+    }
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+
 
 }
