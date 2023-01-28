aad4467d09ef33ca6f13040e7a0eb3665071fcc7
hwstar-1204 <115150798+hwstar-1204@users.noreply.github.com>
2023-01-28 19:19:48 +0900

    0128hyungwon
    profile xml +

---
+++ app/src/main/java/com/example/healthappttt/Activity/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.healthappttt.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.healthappttt.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
