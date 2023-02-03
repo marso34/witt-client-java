cf0b6c80b696f161cc41bf2d6a50c7618f3a8f55
hwstar-1204 <115150798+hwstar-1204@users.noreply.github.com>
2023-02-03 21:01:46 +0900

    0203
    +기능, xml 최신

--- app/src/main/java/com/example/healthappttt/Fragment/ProflieFragment.java
+++ app/src/main/java/com/example/healthappttt/Fragment/ProflieFragment.java
@@ -2,23 +2,26 @@ package com.example.healthappttt.Fragment;
 
 import static android.content.ContentValues.TAG;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideContext;
 import com.example.healthappttt.Data.User;
 import com.example.healthappttt.R;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -80,12 +83,20 @@ public class ProflieFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_proflie, container, false);
+        // 아이디 연결
+        FrameLayout ProfileFrame = view.findViewById(R.id.ProfileFrame);
+
+        TextView mytempreture2 = ProfileFrame.findViewById(R.id.MyTempreture2);
+        TextView username = ProfileFrame.findViewById(R.id.UserName);
+        TextView locationname = ProfileFrame.findViewById(R.id.MyLocation);
+        TextView squat = ProfileFrame.findViewById(R.id.squat);
+        TextView bench = ProfileFrame.findViewById(R.id.benchpress);
+        TextView deadlift = ProfileFrame.findViewById(R.id.deadlift);
+        ImageView userImg = ProfileFrame.findViewById(R.id.UserImg);
 
         //CollectionReference -> 파이어스토어의 컬랙션 참조하는 객체
-        DocumentReference productRef = db.collection("users").document("FeXJyKuFvHM2crCAZRG3IgwKvG02");
+        DocumentReference productRef = db.collection("users").document("vE8rAti9AgYMKxGdZwQqBZX1qFv1");
         //get()을 통해서 해당 컬랙션의 정보를 가져옴
-
-
 
         //단일 문서의 내용 검색
         productRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -95,6 +106,20 @@ public class ProflieFragment extends Fragment {
                     DocumentSnapshot document = task.getResult();
                     if( document.exists()) { // 데이터가 존재할 경우
                         Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+
+                        mytempreture2.setText(document.getData().get("userTemperature").toString());
+                        username.setText(document.getData().get("userName").toString());
+                        bench.setText(document.getData().get("bench").toString());
+                        deadlift.setText(document.getData().get("deadlift").toString());
+                        squat.setText(document.getData().get("squat").toString());
+                        locationname.setText(document.getData().get("locationName").toString());
+
+                        Glide.with(getView()).load(document.getData().get("profileImg")).override(100,100).into(userImg);
+
+
+//                        userImg.setImageURI((Uri) document.getData().get("profileImg"));
+//                      document.getData().get("key").toString();
+//                      document.getData().get("GoodTime").toString();
 
                     }else {
                         Log.d(TAG, "No such document!");
