785d46b9cf90803fdaea66227594433a1348e139
hwstar-1204 <hwstar1204@gmail.com>
2023-07-07 06:26:58 +0900

    마이 프로필 수정하기 기능 + 로컬 저장+ 수정하기 버그 고침
    버그: 프로필 수정하기 클릭후 바로 뒤로가기 했을 경우 예외처리

--- app/src/main/java/com/example/healthappttt/Profile/MyProfileEdit.java
+++ app/src/main/java/com/example/healthappttt/Profile/MyProfileEdit.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.healthappttt.Data.PreferenceHelper;
 import com.example.healthappttt.R;
 import com.example.healthappttt.databinding.ActivityMyProfileEditBinding;
 
@@ -22,14 +23,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class MyProfileEdit extends AppCompatActivity {
 
     private ActivityMyProfileEditBinding binding;
+    private PreferenceHelper prefhelper;
     ImageView edit_img;
     ImageButton elbum;
-    Map<String, Object> Edituserdata;
+    Map<String, Object> Editeduserdata;
     String name;
     int height,weight,gender;
     int squatValue,benchValue,deadValue;
     AtomicInteger squatValue1,benchValue1,deadValue1,WeightSum1;
-
+    Map<String, Object> UpdateDefault;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +41,7 @@ public class MyProfileEdit extends AppCompatActivity {
         binding = ActivityMyProfileEditBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        prefhelper = new PreferenceHelper("default_user_info",this);
         edit_img = findViewById(R.id.edit_img); // 내 프로필 사진
         elbum = findViewById(R.id.elbum);// 사진첩
         //3대 측정, 스쿼트, 벤치, 데드
@@ -58,6 +61,7 @@ public class MyProfileEdit extends AppCompatActivity {
         squatValue1 = new AtomicInteger(squatValue);
         benchValue1 = new AtomicInteger(benchValue);
         deadValue1 = new AtomicInteger(deadValue);
+        WeightSum1 = new AtomicInteger(squatValue+benchValue+deadValue);
         Log.d("MyProfileEdit 스쿼트: ", String.valueOf(squatValue1));
         Log.d("MyProfileEdit 벤치: ", String.valueOf(benchValue1));
         Log.d("MyProfileEdit 데드: ", String.valueOf(deadValue1));
@@ -84,22 +88,66 @@ public class MyProfileEdit extends AppCompatActivity {
         setWeightUpdateListener(binding.SquatUpBtn, binding.SquatDownBtn, binding.SquatText, squatValue1);
         setWeightUpdateListener(binding.BenchUpBtn, binding.BenchDownBtn, binding.BenchText, benchValue1);
         setWeightUpdateListener(binding.DeadUpBtn,binding.DeadDownBtn,binding.DeadText, deadValue1);
+        //수정하기 버튼 누르기
+        UpdateMyProfile();
+
+    }
+    //수정하기 버튼
+    public void UpdateMyProfile() {
+        binding.move3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                /**
+                 * 로컬 저장하는 로직 + 내 프로필로 돌아갔을 때 업데이트되어 보여짐
+                 */
+                UpdateDefault = new HashMap<>();
+                UpdateDefault.put("name",binding.Ename.getText().toString());
+                UpdateDefault.put("height", Integer.valueOf(String.valueOf(binding.Eheight.getText())));
+                UpdateDefault.put("weight", Integer.valueOf(String.valueOf(binding.Eweight.getText())));
+                UpdateDefault.put("squatValue", squatValue1.get());
+                UpdateDefault.put("benchValue", benchValue1.get());
+                UpdateDefault.put("deadValue", deadValue1.get());
+                if(binding.gender.getText().equals("남자")) {
+                    UpdateDefault.put("gender",0);
+                }else{
+                    UpdateDefault.put("gender",1);
+                }
+
+                prefhelper.putUserDefault(UpdateDefault);
+
+//                intent.putExtra("name",binding.Ename.getText().toString());
+//                intent.putExtra("height",Integer.valueOf(String.valueOf(binding.Eheight.getText())) );
+//                intent.putExtra("weight",Integer.valueOf(String.valueOf(binding.Eweight.getText())) );
+//                intent.putExtra("squatValue",squatValue1.get() );
+//                intent.putExtra("benchValue",benchValue1.get() );
+//                intent.putExtra("deadValue",deadValue1.get() );
+//                if(binding.gender.getText().equals("남자")) {
+//                    intent.putExtra("gender",0);
+//                }else{
+//                    intent.putExtra("gender",1);
+//                }
+
+                setResult(RESULT_OK, intent);//응답코드 -1
+                finish();//스택에서 제거
+            }
+        });
     }
 
     public void setWeightUpdateListener(Button upButton, Button downButton,TextView TextValue,AtomicInteger weightValue ) {
         upButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextValue.setText(weightValue.addAndGet(5));
-                binding.Total.setText(WeightSum1.addAndGet(5));
+                TextValue.setText(String.valueOf(weightValue.addAndGet(5)));
+                binding.Total.setText(String.valueOf(WeightSum1.addAndGet(5)));
             }
         });
 
         downButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextValue.setText(weightValue.addAndGet(-5));
-                binding.Total.setText(WeightSum1.addAndGet(-5));
+                TextValue.setText(String.valueOf(weightValue.addAndGet(-5)));
+                binding.Total.setText(String.valueOf(WeightSum1.addAndGet(-5)));
             }
         });
     }
@@ -111,13 +159,13 @@ public class MyProfileEdit extends AppCompatActivity {
             public void onClick(View v){
                 if (clickState == 1) {
                     // 버튼이 클릭된 상태일 때
-                    gender_btn.setBackgroundColor(Color.parseColor("#FFC0CB")); // 핑크색
+                    gender_btn.setTextColor(Color.parseColor("#FFC0CB"));// 핑크색
                     gender_btn.setText("여자");
                     gender = 1;
                     clickState = 0;
                 } else {
                     // 버튼이 클릭되지 않은 상태일 때
-                    gender_btn.setBackgroundColor(Color.parseColor("#0000FF")); // 파란색
+                    gender_btn.setTextColor(Color.parseColor("#0000FF"));// 파란색
                     gender_btn.setText("남자");
                     gender = 0;
                     clickState = 1;
@@ -127,41 +175,34 @@ public class MyProfileEdit extends AppCompatActivity {
     }
 
     //설정하기
-    public void set_btn(View view) {
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                /**
-                 * 로컬 저장하는 로직 + 내 프로필로 돌아갔을 때 업데이트되어 보여짐
-                 */
-                Edituserdata = new HashMap<>();
-                Edituserdata.put("name",name);
-                Edituserdata.put("gender",gender);
-                Edituserdata.put("height", height);//
-                Edituserdata.put("weight", weight);//
-                Edituserdata.put("squatValue", squatValue1.get());
-                Edituserdata.put("benchValue", benchValue1.get());
-                Edituserdata.put("deadValue", deadValue1.get());
-
-                Bundle bundle = new Bundle();
-                for (Map.Entry<String, Object> entry : Edituserdata.entrySet()) {
-                    String key = entry.getKey();
-                    Object value = entry.getValue();
-
-                    if (value instanceof String) {
-                        bundle.putString(key, (String) value);
-                    } else if (value instanceof Integer) {
-                        bundle.putInt(key, (Integer) value);
-                    }
-                    // 다른 데이터 타입에 따라 추가적인 처리를 할 수 있습니다.
-                }
-                intent.putExtras(bundle);
-                setResult(RESULT_OK, intent);//응답코드 -1
-                finish();//스택에서 제거
-            }
-        });
-    };
+//    public void set_btn(View view) {
+//        view.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent();
+//                /**
+//                 * 로컬 저장하는 로직 + 내 프로필로 돌아갔을 때 업데이트되어 보여짐
+//                 */
+//
+//
+//
+//                intent.putExtra("name",binding.Ename.getText().toString());
+//                intent.putExtra("height",Integer.valueOf(String.valueOf(binding.Eheight.getText())) );
+//                intent.putExtra("weight",Integer.valueOf(String.valueOf(binding.Eweight.getText())) );
+//                intent.putExtra("squatValue",squatValue1.get() );
+//                intent.putExtra("benchValue",benchValue1.get() );
+//                intent.putExtra("deadValue",deadValue1.get() );
+//                if(binding.gender.getText().equals("남자")) {
+//                    intent.putExtra("gender",0);
+//                }else{
+//                    intent.putExtra("gender",1);
+//                }
+//
+//                setResult(RESULT_OK, intent);//응답코드 -1
+//                finish();//스택에서 제거
+//            }
+//        });
+//    };
 
 
 }
