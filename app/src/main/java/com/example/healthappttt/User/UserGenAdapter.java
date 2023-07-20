d8d1a8b7adfc2299e9990bf7bd679c324b140cb3
marso34 <96871583+marso34@users.noreply.github.com>
2023-07-20 21:14:14 +0900

    후기 만드는 중

--- app/src/main/java/com/example/healthappttt/User/UserGenAdapter.java
+++ app/src/main/java/com/example/healthappttt/User/UserGenAdapter.java
@@ -4,49 +4,113 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.healthappttt.Data.Chat.UserChat;
+import com.example.healthappttt.Data.User.WittListData;
 import com.example.healthappttt.R;
+import com.google.android.material.card.MaterialCardView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserGenAdapter extends RecyclerView.Adapter<UserGenAdapter.MainViewHolder> {
     private Context context;
 
-    private ArrayList<?> users;
+    private ArrayList<UserChat> chatUsers;
+    private ArrayList<BlockUserAdapter> BlockUsers;
+    private ArrayList<WittListData> WittUsers;
+    private ArrayList<UserChat> SelectUsers;
 
-    public UserGenAdapter(Context context, ArrayList<?> users) {
+    private boolean[] Outline;
+    private int Type;
+
+    public UserGenAdapter(Context context, ArrayList<?> users, int Type) {
         this.context = context;
-        this.users = users;
-    }
+        this.Type = Type;
+        switch (Type) {
+            case 0: this.BlockUsers  = (ArrayList<BlockUserAdapter>) users; break;
+            case 1: this.chatUsers   = (ArrayList<UserChat>)         users; break;
+            case 2: this.WittUsers   = (ArrayList<WittListData>)     users; break;
+            case 3: this.SelectUsers = (ArrayList<UserChat>)         users; break;
+            default: break;
+        }
 
+        this.Outline = new boolean[users.size()];
+    }
 
     public static class MainViewHolder extends RecyclerView.ViewHolder {
+        public MaterialCardView CardView;
+        public View NotifyView;
+        public CircleImageView UserImg;
+        public TextView UserName, UserInfo, ChatTime;
 
-        public MainViewHolder(@NonNull View itemView) {
-            super(itemView);
+        public MainViewHolder(@NonNull View view) {
+            super(view);
+
+            this.CardView   = view.findViewById(R.id.userCard);
+            this.NotifyView = view.findViewById(R.id.newNotify);
+            this.UserImg    = view.findViewById(R.id.userImg);
+            this.UserName   = view.findViewById(R.id.userName);
+            this.UserInfo   = view.findViewById(R.id.userInfo);
+            this.ChatTime   = view.findViewById(R.id.chatTime);
         }
     }
-
 
     @NonNull
     @Override
     public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_user_gen, null);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_user_gen, parent, false);
         final MainViewHolder mainViewHolder = new MainViewHolder(view);
+
+        mainViewHolder.CardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = mainViewHolder.getAbsoluteAdapterPosition();
+
+                if (Type == 3) {
+                    Outline[position] = !Outline[position];
+                    setOutline(position, Outline[position]);
+                }
+            }
+        });
 
         return mainViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+        if (Outline[position])  holder.CardView.setStrokeWidth(2); // 아웃라인 키고 끄기
+        else                    holder.CardView.setStrokeWidth(0);
 
+        // type에 따라서 조절
+//        holder.UserImg. // 이미지 할당
+//        holder.UserName.setText(SelectUsers.get(position).getUserNM());
+//        holder.UserInfo.setText(users.get(position).getGYM());
     }
 
     @Override
     public int getItemCount() {
-        return users.size();
+        switch (Type) {
+            case 0: return this.BlockUsers.size();
+            case 1: return this.chatUsers.size();
+            case 2: return this.WittUsers.size();
+            case 3: return this.SelectUsers.size();
+
+        }
+
+        return  0;
+    }
+
+    public void setOutline(int position, boolean isChecked) {
+        this.Outline[position] = isChecked;
+        notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/example/healthappttt/interface_/ServiceApi.java
+++ app/src/main/java/com/example/healthappttt/interface_/ServiceApi.java
@@ -9,6 +9,7 @@ import com.example.healthappttt.Data.Exercise.RoutineData;
 import com.example.healthappttt.Data.User.BlackListData;
 import com.example.healthappttt.Data.User.GetUserInfo;
 import com.example.healthappttt.Data.User.NearUsersData;
+import com.example.healthappttt.Data.User.ReviewData;
 import com.example.healthappttt.Data.User.ReviewListData;
 import com.example.healthappttt.Data.User.UploadResponse;
 import com.example.healthappttt.Data.User.UserClass;
@@ -41,15 +42,11 @@ public interface ServiceApi {
     @POST("/profile/getBlackList")// 차단목록에서 차단 유저 리스트 가져오는 api
     Call<List<BlackListData>> getBlackList(@Body UserKey userKey);
 
-    @POST("/getMSGFromServer")
-    Call<List<MSG>> getMSGFromServer(@Body pkData pk);
-
     @POST("/profile/getReviewList") //받은 후기에서 후기 리스트 가져오는 api
     Call<List<ReviewListData>> getReviewList(@Body UserKey userKey);
 
     @POST("/profile/deleteFromServer") //차단하기에서 차단 항목 삭제하는 api
     Call<Integer> deleteFromServer(@Body UserKey BL_PK);
-
 
     @POST("/profile/getWittHistory")//위트 내역 리스트 가져오기
     Call<List<WittListData>> getWittHistory(@Body UserKey userKey);
@@ -59,7 +56,7 @@ public interface ServiceApi {
 
     @POST("/profile/getOtherProfile")// 상세 프로필 가져오기
     Call<Map<String,Object>> getOtherProfile(@Body UserKey userKey);
-
+//    ----------------------------------------------------------------------------------------------
     @POST("/routine/CreateRoutine")
     Call<List<Integer>> createRoutine(@Body RoutineData data);
 
@@ -74,6 +71,14 @@ public interface ServiceApi {
 
     @POST("/record/RecordExercise")
     Call<List<Integer>> recordExercise(@Body RecordData data);
+//    ----------------------------------------------------------------------------------------------
+
+    @POST("/review/SendReivew")
+    Call<Integer> sendReivew(@Body ReviewData data);
+
+//    ----------------------------------------------------------------------------------------------
+    @POST("/getMSGFromServer")
+    Call<List<MSG>> getMSGFromServer(@Body pkData pk);
 
     @POST("/GetNearUsers")
     Call<List<UserInfo>> GetNearUsers(@Body NearUsersData data);
