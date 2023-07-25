8fe6c5cc42756a30fa2c298a8b33c367598fc23b
marso34 <mars102aq@gmail.com>
2023-07-25 11:47:35 +0900

    루틴 생성 과정 수정

--- app/src/main/java/com/example/healthappttt/Routine/CRSetTimeFragment.java
+++ app/src/main/java/com/example/healthappttt/Routine/CRSetTimeFragment.java
@@ -15,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.healthappttt.R;
 import com.example.healthappttt.databinding.FragmentCrSetTimeBinding;
 
 /**
@@ -24,6 +25,14 @@ import com.example.healthappttt.databinding.FragmentCrSetTimeBinding;
  */
 public class CRSetTimeFragment extends Fragment {
     FragmentCrSetTimeBinding binding;
+
+    private static final String Backgrount_2 = "#D1D8E2";
+    private static final String Signature = "#05C78C";
+    private static final String Orange = "#FC673F";
+    private static final String Yellow = "#F2BB57";
+    private static final String Blue = "#579EF2";
+    private static final String Purple = "#8C5AD8";
+    private static final String White = "#ffffff";
 
     private int[] schedule;
     private int runTime, startTime, endTime;
@@ -89,13 +98,12 @@ public class CRSetTimeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_cr_set_time, container, false);
-        binding = FragmentCrSetTimeBinding.inflate(inflater);
-
         if (getArguments() != null) {
             schedule = getArguments().getIntArray("schedule");
             // 프래그먼트 시작할 때 시작시간, 종료시간을 넘겨 받을 떄 동장 정의
         }
+
+        binding = FragmentCrSetTimeBinding.inflate(inflater);
 
         return binding.getRoot();
     }
@@ -104,55 +112,19 @@ public class CRSetTimeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        init();
+        binding.morning.setOnClickListener(v -> {
 
-        binding.startTimeUP.setOnClickListener(v -> {
-            if (startTime < endTime) {
-                startTime += 5;
-                runTime = endTime - startTime;
-
-                binding.startTime.setText(TimeToString(startTime));
-                binding.runTime.setText(RuntimeToString(runTime));
-                setNextBtn();
-            }
         });
 
-        binding.startTimeDown.setOnClickListener(v -> {
-            if (startTime > 0) {
-                startTime -= 5;
-                runTime = endTime - startTime;
+        binding.afternoon.setOnClickListener(v -> {});
 
-                binding.startTime.setText(TimeToString(startTime));
-                binding.runTime.setText(RuntimeToString(runTime));
-                setNextBtn();
-            }
-        });
+        binding.evening.setOnClickListener(v -> {});
 
-        binding.endTimeUP.setOnClickListener(v -> {
-            if (endTime < 240) {
-                endTime += 5;
-                runTime = endTime - startTime;
-
-                binding.endTime.setText(TimeToString(endTime));
-                binding.runTime.setText(RuntimeToString(runTime));
-                setNextBtn();
-            }
-        });
-
-        binding.endTimeDown.setOnClickListener(v -> {
-            if (endTime > startTime) {
-                endTime -= 5;
-                runTime = endTime - startTime;
-
-                binding.endTime.setText(TimeToString(endTime));
-                binding.runTime.setText(RuntimeToString(runTime));
-                setNextBtn();
-            }
-        });
+        binding.dawn.setOnClickListener(v -> {});
 
         binding.nextBtn.setOnClickListener(v -> {
             if (runTime > 0) {
-                mListener.onRoutineSetTime(startTime, endTime);
+//                mListener.onRoutineSetTime(startTime, endTime);
             }
         });
     }
@@ -164,68 +136,13 @@ public class CRSetTimeFragment extends Fragment {
         binding = null; //바인딩 객체를 GC(Garbage Collector) 가 없애도록 하기 위해 참조를 끊기
     }
 
-    private void init() {
-        startTime = TimeToInt((String) binding.startTime.getText());
-        endTime = TimeToInt((String) binding.endTime.getText());
-
-        Log.d("운동 시간", startTime + " " + endTime);
-        runTime = endTime - startTime;
-    }
-
     private void setNextBtn() {
         if (runTime > 0) {
-//            NextBtn = (CardView) view.findViewById(R.id.nextBtn);
-            binding.nextTxt.setBackgroundColor(Color.parseColor("#05c78c"));
-            binding.nextTxt.setTextColor(Color.parseColor("#ffffff"));
+            binding.nextBtn.setBackground(getContext().getDrawable(R.drawable.rectangle_green_20dp));
+            binding.nextBtn.setTextColor(Color.parseColor(White));
         } else {
-            binding.nextTxt.setBackgroundColor(Color.parseColor("#D1D8E2"));
-            binding.nextTxt.setTextColor(Color.parseColor("#9AA5B8"));
+//            binding.nextBtn.setBackgroundColor(Color.parseColor("#D1D8E2"));
+//            binding.nextBtn.setTextColor(Color.parseColor("#9AA5B8"));
         }
-    }
-
-    private int TimeToInt(String Time) { // 시작 시간 및 종료 시간을 전역으로 가지고 종료시간이 시작 시간보다 빠를 수 없도록 수정할 것
-        String am_pm = Time.substring(0, Time.indexOf(" "));
-        String temp = Time.substring(Time.indexOf(" ")+1);
-        String hour = temp.substring(0, temp.indexOf(":"));
-        String min = temp.substring(temp.indexOf(":")+1);
-
-        int T = Integer.parseInt(hour) * 10 + Integer.parseInt(min) / 6;
-
-        if (am_pm.equals("오후")) T += 120;
-        if (T == 120 || T == 125 || T == 240 || T == 245) T-= 120;
-
-        return T;
-    }
-
-    private String TimeToString(int Time) {
-        String am_pm = "";
-
-        if (Time >= 240) Time-= 240;
-
-        if (Time < 120) {
-            am_pm = "오전";
-            if (Time < 10) Time += 120;
-        } else {
-            am_pm = "오후";
-            if (Time >= 130) Time-= 120;
-        }
-
-        @SuppressLint("DefaultLocale") String result = String.format("%02d:%02d", Time/10, Time % 10 * 6);
-
-        return am_pm + " " + result;
-    }
-
-    private String RuntimeToString(int Time) {
-        @SuppressLint("DefaultLocale") String result1 = String.format("%d분", Time % 10 * 6);
-        @SuppressLint("DefaultLocale") String result2 = String.format("%d시간", Time/10);
-        @SuppressLint("DefaultLocale") String result3 = String.format("%d시간 %d분", Time/10, Time % 10 * 6);
-
-        if (Time < 10)
-            return result1;
-
-        if (Time % 10 == 0)
-            return result2;
-
-        return result3;
     }
 }
